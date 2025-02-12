1b70788fbbab6c8dc75d7337aa0ac3bb74a74d4c
cristianrs-dev <cris.tecwordd@gmail.com>
2025-02-12 16:48:36 -0400

    efeito css float adicionado

--- js/script.js
+++ js/script.js
@@ -21,3 +21,26 @@ document.addEventListener('DOMContentLoaded', () => {
     
 });
 
+// Seleciona todos os cards
+const cards = document.querySelectorAll('.item-card-skill');
+
+// Função para verificar se o card está bem visível na tela
+function checkVisibility() {
+  cards.forEach(card => {
+    const cardPosition = card.getBoundingClientRect(); // Pega a posição do card na tela
+
+    // Verifica se pelo menos 80% do card está visível na tela
+    if (cardPosition.top < window.innerHeight * 0.8 && cardPosition.bottom >= window.innerHeight * 0.2) {
+      card.classList.add('visible'); // Adiciona a classe 'visible' para ativar a animação
+    } else {
+      card.classList.remove('visible'); // Remove a classe 'visible' quando o card não estiver suficientemente visível
+    }
+  });
+}
+
+// Chama a função sempre que a página for rolada
+window.addEventListener('scroll', checkVisibility);
+
+// Chama a função uma vez no carregamento para verificar se algum card já está visível
+checkVisibility();
+
